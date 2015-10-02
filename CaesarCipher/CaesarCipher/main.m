ce9489e3fdfb6c75c5e8c4bdaf0c422cc86df813
kaishajones <kaisha_jns@yahoo.com>
2015-10-02 02:58:15 -0400

    Cipher Done

--- CaesarCipher/CaesarCipher/main.m
+++ CaesarCipher/CaesarCipher/main.m
@@ -12,7 +12,7 @@
 
 - (NSString *)encode:(NSString *)string offset:(int)offset;
 - (NSString *)decode:(NSString *)string offset:(int)offset;
-
+- (BOOL)codeBreaker:(NSString *)string1 withStrings:(NSString *)string2;
 @end
 
 
@@ -29,7 +29,7 @@
     [str getCharacters:buffer range:NSMakeRange(0, count)];
     
     char allchars[] = "abcdefghijklmnopqrstuvwxyz";
-
+    
     for (int i = 0; i < count; i++) {
         if (buffer[i] == ' ' || ispunct(buffer[i])) {
             result[i] = buffer[i];
@@ -39,10 +39,10 @@
         char *e = strchr(allchars, buffer[i]);
         int idx= (int)(e - allchars);
         int new_idx = (idx + offset) % strlen(allchars);
-
+        
         result[i] = allchars[new_idx];
     }
-
+    
     return [NSString stringWithCharacters:result length:count];
 }
 
@@ -50,11 +50,49 @@
     return [self encode:string offset: (26 - offset)];
 }
 
+- (BOOL)codeBreaker:(NSString *)string1 withStrings:(NSString *)string2 {
+    
+    BOOL result = NO;
+    
+    for (int i = 1; i <= 25; i++) {
+        
+        NSString * decodedString = [self decode:string1 offset:i];
+        
+        for (int j = 1; j <= 25; j++) {
+            
+            NSString * decodedString2 = [self decode:string2 offset:j];
+            
+            if ([decodedString isEqualToString:decodedString2]) {
+                
+                result = YES;
+                
+                NSLog(@"%@ decoded is %@ using offset %d", string1, decodedString, i);
+                
+                NSLog(@"%@ decoded is %@ using offset %d", string2, decodedString2, j);
+                
+                return result;
+            }
+        }
+    }
+    
+    return result;
+}
+
 @end
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        CaesarCipher *cipherInstance = [[CaesarCipher alloc]init];
+        
+        NSString *kaisha = @"kaisha";
+        NSString *jones = @"jones";
+        NSString *encodedKai1 = [cipherInstance encode:kaisha offset:2];
+        NSString *encodedKai2 = [cipherInstance encode:jones offset:3];
+        
+        BOOL isSame = [cipherInstance codeBreaker:encodedKai1 withStrings:encodedKai2];
+        
+        NSLog(@"Are these ciphers the same? %s", isSame ? "YES":"NO");
         
     }
 }
